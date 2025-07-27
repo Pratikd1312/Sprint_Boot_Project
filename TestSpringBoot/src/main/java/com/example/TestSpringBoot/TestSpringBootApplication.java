f20fc23173126ccba2bb7ccdf4ba232e205365f9
First Project <pratikdhurde23@gmail.com>
2025-07-27 11:48:46 +0530

    Commit to generate war

--- TestSpringBoot/src/main/java/com/example/TestSpringBoot/TestSpringBootApplication.java
+++ TestSpringBoot/src/main/java/com/example/TestSpringBoot/TestSpringBootApplication.java
@@ -2,12 +2,18 @@ package com.example.TestSpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TestSpringBootApplication {
+public class TestSpringBootApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestSpringBootApplication.class, args);
 	}
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(TestSpringBootApplication.class);
+    }
 }
